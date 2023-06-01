bff4c34c4313ce051c322143f8908b456421f548
dylanbrine <dylan.brine@milkmoovement.com>
2023-06-01 10:08:45 -0300

    added repo calls to cs service

--- src/services/character-sheet-service/character-sheet.service.ts
+++ src/services/character-sheet-service/character-sheet.service.ts
@@ -1,33 +1,30 @@
+import { CharacterSheet } from '../../database/models/character-sheet/character-sheet.model';
+import { CharacterSheetRepository } from '../../database/repositories/character-sheet.repository';
+
 export default class CharacterSheetService {
+  serviceRepository: CharacterSheetRepository;
   constructor() {
-    // do nothing
+    this.serviceRepository = new CharacterSheetRepository();
   }
 
   public getCharacterSheet = async (id: string): Promise<CharacterSheet> => {
-    return new CharacterSheet();
+    return await this.serviceRepository.getCharacterSheetById(id);
   };
 
   public createCharacterSheet = async (
     characterSheet: CharacterSheet
   ): Promise<CharacterSheet> => {
-    return new CharacterSheet();
+    return await this.serviceRepository.createCharacterSheet(characterSheet);
   };
 
   public updateCharacterSheet = async (
     id: string,
     characterSheet: CharacterSheet
   ): Promise<CharacterSheet> => {
-    return new CharacterSheet();
-  };
-
-  public replaceCharacterSheet = async (
-    id: string,
-    characterSheet: CharacterSheet
-  ): Promise<CharacterSheet> => {
-    return new CharacterSheet();
+    return await this.serviceRepository.updateCharacterSheet(characterSheet);
   };
 
   public deleteCharacterSheet = async (id: string): Promise<CharacterSheet> => {
-    return new CharacterSheet();
+    return await this.serviceRepository.deleteCharacterSheet(id);
   };
 }
