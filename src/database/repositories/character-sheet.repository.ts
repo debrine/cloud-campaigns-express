183a52d9989879c50c9116818674c459287445f3
dylanbrine <dylan.brine@milkmoovement.com>
2023-06-01 09:49:01 -0300

    add character sheet repo stubs

---
+++ src/database/repositories/character-sheet.repository.ts
@@ -0,0 +1,14 @@
+export class CharacterSheetRepository {
+  // add in table prefixes and other things here
+  constructor() {}
+
+  async getCharacterSheetById(id: string) {}
+
+  async getCharacterSheets() {}
+
+  async createCharacterSheet(characterSheet: any) {}
+
+  async updateCharacterSheet(characterSheet: any) {}
+
+  async deleteCharacterSheet(id: string) {}
+}
